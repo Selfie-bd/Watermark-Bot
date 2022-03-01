75a9da24f7e7af9f625697c3e23f803aec97671d
Selfie SD <webtamil0@gmail.com>
2022-03-01 15:44:56 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,19 +8,19 @@ import os
 
 
 class Config(object):
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	API_ID = int(os.environ.get("API_ID", 12345))
-	API_HASH = os.environ.get("API_HASH")
-	STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS", "NoNeed")
-	STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME", "NoNeed")
-	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
+	BOT_TOKEN = os.environ.get("BOT_TOKEN","5157031470:AAGCLotL43DhZWemzsxgH5Jf02VeTtMw3nQ")
+	API_ID = int(os.environ.get("API_ID", 1976680))
+	API_HASH = os.environ.get("API_HASH","9073255ce64a6072a59099803493f97d")
+	STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS", "JkqQxXVQZzuj3DM")
+	STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME", "5e4f1b21f9638094a8cf")
+	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL","-1001611517455"))
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
 	DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
 	PRESET = os.environ.get("PRESET", "ultrafast")
-	OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714))
-	CAPTION = "By @AHToolsBot"
-	BOT_USERNAME = os.environ.get("BOT_USERNAME", "VideoWatermark_Bot")
-	DATABASE_URL = os.environ.get("DATABASE_URL")
+	OWNER_ID = int(os.environ.get("OWNER_ID", 1940030638))
+	CAPTION = "By @GroupDcBots"
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "WatermarkDcBot")
+	DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://erichdaniken:erichdaniken@cluster0.c13qk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 	ALLOW_UPLOAD_TO_STREAMTAPE = bool(os.environ.get("ALLOW_UPLOAD_TO_STREAMTAPE", True))
 	USAGE_WATERMARK_ADDER = """
@@ -29,9 +29,9 @@ Hi, I am Video Watermark Adder Bot!
 **How to Added Watermark to a Video?**
 **Usage:** First Send a JPG Image/Logo, then send any Video. Better add watermark to a MP4 or MKV Video.
 
-__Note: I can only process one video at a time. As my server is Heroku, my health is not good. If you have any issues with Adding Watermark to a Video, then please Report at [Support Group](https://t.me/linux_repo).__
+__Note: I can only process one video at a time. As my server is Heroku, my health is not good. If you have any issues with Adding Watermark to a Video, then please Report at [Support Group](https://t.me/Groupdc).__
 
-Desgined by @AbirHasan2005
+Desgined by @GroupDcBots
 """
 	PROGRESS = """
 Percentage : {0}%
